536f552d6e705ecb4950e9c4490b19ba6a56104f
tas2017 <tasos.valtinos@gmail.com>
2023-04-27 12:29:14 +0100

    Using Model.Mint to create fake value coins

--- src/project-idea/tests/PTEscrowNFT.hs
+++ src/project-idea/tests/PTEscrowNFT.hs
@@ -8,23 +8,61 @@ module PTEscrowNFT where
 
 import qualified EscrowNFT       as OnChain
 import           Control.Monad           (mapM, replicateM)
+import           Data.String             (fromString)
 import           Plutus.Model            (Ada (Lovelace), DatumMode (HashDatum),
                                           Run, Tx,
                                           TypedValidator (TypedValidator),
-                                          UserSpend, ada, adaValue,
+                                          UserSpend, FakeCoin (..), fakeCoin, fakeValue, ada, adaValue,
                                           defaultBabbage, initMock, mustFail,
                                           newUser, payToKey, payToScript,
                                           runMock, spend, spendScript, submitTx,
                                           toV2, userSpend, utxoAt, valueAt, waitUntil, currentTimeRad, validateIn)
 import           Plutus.V2.Ledger.Api    (PubKeyHash, TxOut (txOutValue),
                                           TxOutRef, Value, POSIXTime (POSIXTime, getPOSIXTime))
-import           PlutusTx.Builtins       (Integer, mkI)
+import           PlutusTx.Builtins       (Integer, BuiltinByteString, encodeUtf8, mkI)
 import           PlutusTx.Prelude        (Bool (..), Eq ((==)),
                                           return, ($), (&&), (.))
-import           Prelude                 (IO, Ord ((<), (>)),
+import           Prelude                 (IO, String, Ord ((<), (>)),
                                           mconcat)
 import           Test.QuickCheck         (Property, Testable (property),
                                           collect, (==>), Arbitrary (arbitrary), choose)
 import           Test.QuickCheck.Monadic (assert, monadic, run)
 import           Test.Tasty              (defaultMain, testGroup)
 import           Test.Tasty.QuickCheck   as QC (testProperty)
+
+
+-- | Helper functions for using Quickcheck
+-- | Testable and Arbitrary instances to generate random cases for the validators parameters 
+
+
+instance Testable a => Testable (Run a) where
+  property rp = let (a,_) = runMock rp $ initMock defaultBabbage (adaValue 100_000_000) in property a
+
+
+-- | Our on chain validator checks 3 things
+-- 1. A matching password
+-- 2. A matching TokenName
+-- 3. If the user is burning the NFT when unlocking the funds 
+-- We will use the FakeCoin, fakecoin, fakevalue functions from Plutus.Model.Mint
+
+
+-- Validator's script
+-- | Versioning the validator to v2
+
+valScript :: TypedValidator datum redeemer
+valScript = TypedValidator $ toV2 OnChain.typedValidator
+
+-- Set many users at once
+setupUsers :: Run [PubKeyHash]
+setupUsers = replicateM 2 $ newUser $ ada (Lovelace 10000000)
+
+toBBString :: String -> PlutusTx.Builtins.BuiltinByteString
+toBBString = PlutusTx.Builtins.encodeUtf8 . fromString
+
+
+nftToken = FakeCoin {
+    fakeCoin'tag = toBBString "LockDaFund"
+}
+
+nftTokenValue :: Value
+nftTokenValue = fakeValue nftToken 1
